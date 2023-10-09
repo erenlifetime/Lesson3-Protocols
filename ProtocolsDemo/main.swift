c3dbbea9e9888e2e66af258f3de99127b5389330
Eren Pekdemir <93570359+erenlifetime@users.noreply.github.com>
2023-10-09 21:50:31 +0300

    Update main.swift

--- ProtocolsDemo/main.swift
+++ ProtocolsDemo/main.swift
@@ -1,11 +1,43 @@
-//
-//  main.swift
-//  ProtocolsDemo
-//
-//  Created by Eren lifetime on 9.10.2023.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+protocol CanTalk{
+    func talk()
+}
+class Alien{
+   var isFemale = true
+    func giveBirth(){
+        if isFemale{
+            print("Female dogs can give birth")
+        }
+    }
+}
+    class People:Alien,CanTalk{
+    func talk() {
+        print("People can talk")
+    }
+    func Chicken(){
+        print("Chickens cant talk")
+    }
+}
+struct Monkey:CanTalk{
+    func talk() {
+        
+    }
+    func monkeyCanTalk(objectif:CanTalk){
+        objectif.talk()
+    }
+}
+struct flyingMuseum{
+    func flyingDemo(flyingObject:CanTalk){
+        flyingObject.talk()
+    }
+}
+struct Parrot:CanTalk{
+    func talk(){
+    print("Parrot can talk.")
+    }
+}
+let myParrot = Parrot()
+let myPeople = People()
+myPeople.giveBirth()
+myParrot.talk()
+let museum = flyingMuseum()
+museum.flyingDemo(flyingObject:People())
